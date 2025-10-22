6b93884fd493a1412abd5fd70574067dd63207d9
akhy22 <425094006@pcpuma.acatlan.unam.mx>
2025-10-22 12:07:22 -0600

    ex2_list

--- data_structure/code/main/list/ex2_list.c
+++ data_structure/code/main/list/ex2_list.c
@@ -36,24 +36,20 @@ int main(int argc, char **argv) {
     char *data;
     int i;
 
-    // Inicializa la lista
     list_init(&list, free);
 
-    // Llena la lista con las letras del alfabeto (A-Z)
     for (i = 0; i < 26; i++) {
         if ((data = (char *)malloc(sizeof(char))) == NULL)
             return 1;
 
         *data = 'A' + i;
 
-        // Insertamos al inicio para mantener el orden inverso
         if (list_ins_next(&list, NULL, data) != 0)
             return 1;
     }
 
     print_list(&list);
 
-    // Eliminar el nodo después del 7º nodo
     node = list_head(&list);
     for (i = 0; i < 7; ++i)
         node = list_next(node);
@@ -66,7 +62,6 @@ int main(int argc, char **argv) {
 
     print_list(&list);
 
-    // Insertar 'Z' al final de la lista
     fprintf(stdout, "\nInserting 'Z' at the tail of the list\n");
     if ((data = (char *)malloc(sizeof(char))) == NULL)
         return 1;
@@ -76,19 +71,16 @@ int main(int argc, char **argv) {
 
     print_list(&list);
 
-    // Eliminar nodo después del primero
     fprintf(stdout, "\nRemoving a node after the first node\n");
     if (list_rem_next(&list, list_head(&list), (void **)&data) != 0)
         return 1;
     print_list(&list);
 
-    // Eliminar el nodo de la cabeza
     fprintf(stdout, "\nRemoving a node at the head of the list\n");
     if (list_rem_next(&list, NULL, (void **)&data) != 0)
         return 1;
     print_list(&list);
 
-    // Insertar un nuevo carácter al inicio
     fprintf(stdout, "\nInsert 'X' at the head of the list\n");
     if ((data = (char *)malloc(sizeof(char))) == NULL)
         return 1;
@@ -97,7 +89,6 @@ int main(int argc, char **argv) {
         return 1;
     print_list(&list);
 
-    // Probar macros de verificación
     i = list_is_head(&list, list_head(&list));
     fprintf(stdout, "\nTesting list_is_head... value=%d (1=OK)\n", i);
     i = list_is_head(&list, list_tail(&list));
@@ -107,10 +98,8 @@ int main(int argc, char **argv) {
     i = list_is_tail(list_head(&list));
     fprintf(stdout, "Testing list_is_tail... value=%d (0=OK)\n", i);
 
-    // Destruir la lista
     fprintf(stdout, "\nDestroying the list\n");
     list_destroy(&list);
 
     return 0;
 }
-
